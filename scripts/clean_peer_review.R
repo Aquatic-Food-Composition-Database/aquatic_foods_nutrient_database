95d79ba974fb11d0b3d8468a7cbf6698a213b7a5
Zach Koehn <zachkoehn@Zachs-MacBook-Pro.local>
2020-10-08 16:34:56 -0700

    clean up filepaths

--- scripts/clean_peer_review.R
+++ scripts/clean_peer_review.R
@@ -13,32 +13,34 @@
 ##################
 library(tidyverse);library(here);library(data.table)
 
+directory <- "/Volumes/GoogleDrive/My Drive/BFA_Papers/BFA_Nutrition/Separate/aquatic_foods_nutrient_database"
+
 
 # load nutrient information from Camille's datasets in our drive folder
 # note this uses the here package, which locates the files based on whatever folder the Rproject is stored in
 macro <- read.csv(
-  here("data","afcd_peer_review_data","Seafood nutrients","macro nutrients.csv"),
+  file.path(directory,"data","afcd_peer_review_data","Seafood nutrients","macro nutrients.csv"),
   header=TRUE
 )
 amino <- read.csv(
-  here("data","afcd_peer_review_data","Seafood nutrients","amino acids.csv"),
+  file.path(directory,"data","afcd_peer_review_data","Seafood nutrients","amino acids.csv"),
   header=TRUE
   )
 fats <- read.csv(
-  here("data","afcd_peer_review_data","Seafood nutrients","fatty_acids.csv"),
+  file.path(directory,"data","afcd_peer_review_data","Seafood nutrients","fatty_acids.csv"),
   header=TRUE
 ) %>%
   dplyr::select(-X)
 minerals <- read.csv(
-  here("data","afcd_peer_review_data","Seafood nutrients","minerals.csv"),
+  file.path(directory,"data","afcd_peer_review_data","Seafood nutrients","minerals.csv"),
   header=TRUE
 )
 misc <- read.csv(
-  here("data","afcd_peer_review_data","Seafood nutrients","misc..csv"),
+  file.path(directory,"data","afcd_peer_review_data","Seafood nutrients","misc..csv"),
   header=TRUE
 )
 vitamin <- read.csv(
-  here("data","afcd_peer_review_data","Seafood nutrients","vitamins.csv"),
+  file.path(directory,"data","afcd_peer_review_data","Seafood nutrients","vitamins.csv"),
   header=TRUE
 )
 
@@ -48,7 +50,7 @@ vitamin <- read.csv(
 ##################
 
 name_intersect <- Reduce(intersect,
-                         list(
+                         tibble::lst(
                            names(macro),
                            names(amino),
                            names(fats),
@@ -98,6 +100,9 @@ all_nutrients_4 <- full_join(
 ##################
 # for now REMOVE relative values, those are cleaned & calculated in "clean_pere_review_relative_values.R"
 ##################
+
+names(all_nutrients_4)[1] <- "Study.ID.number"
+
 all_nutrients_no_relatives <- all_nutrients_4 %>%
   select(
     -str_subset(names(all_nutrients_4),"PAA."), #for now removes 
@@ -119,7 +124,7 @@ all_nutrients_no_relatives <- all_nutrients_4 %>%
 
 write.csv(
   all_nutrients_no_relatives,
-  here("data","OutputsFromR","cleaned_fcts",
+  file.path(directory,"data","OutputsFromR","cleaned_fcts",
        "clean_peer_review.csv"
        ),
   row.names = FALSE
