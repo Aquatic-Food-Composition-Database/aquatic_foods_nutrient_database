10d5c35111b319d829dcc5489e64f039e9ae460d
Zach Koehn <zachkoehn@COS-NJ17DQLWG7.local>
2022-10-13 16:42:29 -0700

    cleaned usda api pull data for merge with AFCD

--- scripts/clean_usda.R
+++ scripts/clean_usda.R
@@ -9,9 +9,10 @@
 # ===============================================================================
 library(tidyverse)
 library(here)
+library(readxl)
 
 raw_usda <- read_csv(file=here("data","usa","usda_api_extract.csv"))
-
+source(here("scripts","functions","func_cleaning_fct.R"))
 # and read the excel merge key used to modify names and units to merge with AFCD
 merge_key <- read_excel(
   here("data","database_merge_key.xlsx"),
@@ -19,19 +20,19 @@ merge_key <- read_excel(
 )
 # first export variables names and add to the excel file with the key to merge/transform names
 # "database_merge_key.xlsx"
-usda_nutrient_info <- rbind(
-  data.frame(
-    variable_name=c("fdc_id","food_description","scientific_name"),
-    nutrient_unit="none"
-    ),
-  data.frame(
-    variable_name=raw_usda$nutrient_name,
-    nutrient_unit=raw_usda$nutrient_unit
-    )
-  ) %>%
-  distinct() %>%
-  drop_na(variable_name) %>%
-  write_csv(here("data","outputsFromR","usda_variable_names.csv"))
+# usda_nutrient_info <- rbind(
+#   data.frame(
+#     variable_name=c("fdc_id","food_description","scientific_name"),
+#     nutrient_unit="none"
+#     ),
+#   data.frame(
+#     variable_name=raw_usda$nutrient_name,
+#     nutrient_unit=raw_usda$nutrient_unit
+#     )
+#   ) %>%
+#   distinct() %>%
+#   drop_na(variable_name) %>%
+#   write_csv(here("data","outputsFromR","usda_variable_names.csv"))
 
 usda_clean <- raw_usda %>%
   # clean scientific names, remove names of taxonomists from scientific name for merge with AFCD
@@ -48,24 +49,64 @@ usda_clean <- raw_usda %>%
     scientific_name = str_replace_all(scientific_name," Lamarck",""),# remove taxonomist identifer
     scientific_name = str_replace_all(scientific_name," Rathbun",""),# remove taxonomist identifer
     scientific_name = str_replace_all(scientific_name," Dana",""),# remove taxonomist identifer
-    scientific_name= str_trim(scientific_name,side=c("both")) # trim food names
+    scientific_name= str_trim(scientific_name,side=c("both")), # trim food names
+    nutrient_name=ifelse((nutrient_name=="Energy" & nutrient_unit=="kcal"),"Energy kcal",nutrient_name),
+    nutrient_name=ifelse((nutrient_name=="Energy" & nutrient_unit=="kJ"),"Energy kj",nutrient_name)
     ) %>%
-  merge(
-    
-  )
+  drop_na(nutrient_value) %>%
+  select(-nutrient_unit) %>%
+  distinct() %>%
+  pivot_wider(names_from=nutrient_name,values_from=nutrient_value)
+
 # modify nutrient names for merge with AFCD
   
-  
+# run user-written function that converts nutrient measurement units if needed
 
-# extract specific product types
-raw.usda.data <- usda.relevant.data[grep(", raw", usda.relevant.data$food.name), ]
-canned.usda.data <- usda.relevant.data[grep(", canned", usda.relevant.data$food.name), ]
-cooked.usda.data <- usda.relevant.data[grep(", cooked", usda.relevant.data$food.name), ]
-baked.usda.data <- usda.relevant.data[grep(", baked", usda.relevant.data$food.name), ]
+usda_conversion_coefs <- coefs_convert_unit_fct(
+  key=merge_key,
+  original_unit="usda_unit",
+  convert_to_unit="AFCD_unit",
+  variables_to_convert="usda_variable_name"
+  ) %>%
+  slice(-1:-3,) %>%
+  select(usda_variable_name,coefs) #just select the variable name and conversion coefficient
+
+# now take all that information and create a named vector, with JUST the conversion coefficients named by the nutrient to be converted
+coefs <- as.numeric(usda_conversion_coefs$coefs)
+usda_names <- usda_conversion_coefs$usda_variable_name
+names(coefs) <- usda_names #names it
+
+# now figure out which variables match from the conversion vector to 
+name_match <- intersect(names(usda_clean), names(coefs))
+usda_clean[name_match] <- sweep(usda_clean[name_match], 2, unlist(coefs[name_match]), `*`)
+
+
+
+# finally, change names so that it can be readily merged with AFCD
+
+# use function in "functions/func_cleaning_fct.r" create dataframe that 
+# includes variable names to change from usda to AFCD
+# note: usda Sodium variable was called "NA", which doesn't play nice in programming so it was modified in
+# the CSV to "SODIUM"
+
+usda_names_to_convert_to_afcd <- convert_nutrient_names("usda_variable_name") 
+
+
+usda_aquatic_foods_dat_clean <- usda_clean %>%
+  data.table::setnames(
+    old=usda_names_to_convert_to_afcd$original_dataset,
+    new=usda_names_to_convert_to_afcd$AFCD_variable_name,
+    skip_absent=TRUE)
+# if this throws an error related to teh old not being int he new... it's usually because the formatting of those 
+# variables is wrong. 
+
+usda_aquatic_foods_dat_clean$Country.ISO3 <- "USA_USDA_2022" #adds classification for  usda
+
+
 
 # and store
-write.csv(all.usda.relevant.data,
-          here("OutputsFromR","raw.usda.data.csv")
+write_csv(usda_aquatic_foods_dat_clean,
+          here("data","OutputsFromR","cleaned_fcts","clean_usda_food_data_central.csv")
 )
 
 
